0c796c843a50511b25b4ddad0377f85684228610
r2DoesInc <r2doesinc@gmail.com>
2011-06-07 21:44:08 -0500

    Update changelog and thanks.

--- src/com/android/settings/ThanksDonators.java
+++ src/com/android/settings/ThanksDonators.java
@@ -9,7 +9,7 @@ public class ThanksDonators extends Activity {
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        TextView tv = new TextView(this);
-       tv.setText("-Bought me a developer Incredible, allowing me to sell my old one and buy a Thunderbolt as well. Thank you all so much.");
+       tv.setText("-Bought me a developer Incredible, allowing me to sell my old one and buy a Thunderbolt as well. Thank you all so much." + "Just recently provided us with THREE Fascinates! Thank you birdman, Camdogxiii, and lucious584 so much!");
        setContentView(tv);
    }
 }
